a0bf2e91e77361ef32560e86c29ff557f0461c61
Matthew Janaszczyk <janaszczyk@googlemail.com>
2021-01-23 17:03:11 +0000

    feat: simplified nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,8 +5,7 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
